a8da61a37b6ada1c044a5dba7c29f8c76756a1f8
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-06-24 11:46:49 +0300

    sss

---
+++ index.js
@@ -0,0 +1,68 @@
+// Завдання 1
+// const numbers = [1, 2, 3]; 
+// numbers[1] = 10; 
+// console.log(numbers); 
+
+// Завдання 2(я не знаю як додавати рядки у масив)
+
+
+// Завдання 3
+// const array = [10, 20, 30, 40, 50];
+// let sum = 0;
+
+// for (let number of array) {
+//   sum += number;
+// }
+
+// console.log(sum)
+
+
+// Завдання 4
+
+// const numbers = [1, 2, 3, 4, 5]; 
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]); 
+// }
+
+
+// Завдання 5 (я не знаю що тут не так)
+
+// const friends = [
+//     ["Sasha", "Slavik", "Arsen"],
+//     ["Vlad", "Zhenya"],
+//     ["Saha", "Sofia", "Lera", "Iryna"],
+//     ["Artem", "Dima"],
+//     ["Mark", "Andriy", "David", "Bogdan"]
+// ]; 
+
+// for (let friend of friends) {
+//     if (friend.length > 5) {
+//         console.log(friend); 
+//     }
+// }
+
+
+// Завдання 6
+
+// const numbers = [12, 95, 107, 25, 93, 57, 52, 1008, 2093, 68]; 
+// let maxNumber = numbers[0]; 
+
+// for (let number of numbers) {
+//     if (number > maxNumber) {
+//         maxNumber = number; 
+//     }
+// }
+
+// console.log(maxNumber); 
+
+
+// Завдання 7
+
+const numbers = [20, 13, 93, 66, 77, 18, 27, 90, 52, 18];
+ 
+for (let number of numbers) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+}
